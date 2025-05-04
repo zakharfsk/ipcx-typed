f8df267fd1b43967e6bcb6f7f248878ba80033d7
zakharfsk <fesikzakhar@khmnu.edu.ua>
2025-05-04 20:51:02 +0300

    Update project metadata in pyproject.toml and remove license information from __init__.py
    - Added MIT license information and specified the license file in pyproject.toml.
    - Removed license, copyright, and version details from __init__.py to streamline the module's metadata.

--- ipcx_typed/__init__.py
+++ ipcx_typed/__init__.py
@@ -7,9 +7,3 @@ from .client import Client
 from .server import Server, route
 
 __all__ = ["Client", "Server", "route"]
-
-__title__ = "ipcx_typed"
-__author__ = "zakharfsk"
-__license__ = "Apache-2.0"
-__copyright__ = "(c) 2024-present zakharfsk"
-__version__ = "0.1.0"
